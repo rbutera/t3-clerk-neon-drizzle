643c963f148bb51c40dd6e87cd64bd2dcf609e19
Rai Butera <rai@rbutera.com>
2024-05-21 15:44:31 +0100

    add gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,35 @@
-import Link from "next/link";
+const mockUrls = [
+  "https://utfs.io/f/192a0280-3a37-4bbb-80d8-3adca00c8ce2-zaz7iu.png",
+  "https://utfs.io/f/1a543cbb-620d-43f5-8a29-517e55189f5a-1n164m.jpg",
+  "https://utfs.io/f/2d476b96-29d4-4b9d-a5b1-c1efa9263716-k7uii1.png",
+  "https://utfs.io/f/52e96d94-6015-4cc5-a643-8fae14224fed-etb98h.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Gallery
         </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+        <div className="flex flex-wrap">
+          {mockImages.map((image) => (
+            <div
+              key={image.id}
+              className="w-1/2 p-2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6"
+            >
+              <img
+                src={image.url}
+                alt={`Image ${image.id}`}
+                className="h-auto w-full rounded-lg"
+              />
             </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+          ))}
         </div>
       </div>
     </main>
